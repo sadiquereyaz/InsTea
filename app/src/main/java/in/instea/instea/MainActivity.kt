90ab7ff511d042df897864fc9f8f97d84dd0dcc9
Sadique Reyaz <mdsadique47@gmail.com>
2024-06-28 00:20:32 +0530

    profile ui v1

--- app/src/main/java/in/instea/instea/MainActivity.kt
+++ app/src/main/java/in/instea/instea/MainActivity.kt
@@ -3,11 +3,8 @@ package `in`.instea.instea
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Notifications
@@ -19,19 +16,15 @@ import androidx.compose.material3.Badge
 import androidx.compose.material3.BadgedBox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.NavHost
@@ -42,28 +35,22 @@ import `in`.instea.instea.screens.Notificaiton
 import `in`.instea.instea.screens.Profile
 import `in`.instea.instea.screens.SCREENS
 import `in`.instea.instea.ui.theme.InsTeaTheme
-//import in.instea.instea.screens.Feed
-//import in.instea.instea.screens.Notificaiton
-//import in.instea.instea.screens.Profile
-//import in.instea.instea.screens.SCREENS
-//import in.instea.instea.ui.theme.InsTeaTheme
 
 data class BottomNavItem(
-    val title:String,
+    val title: String,
     val selectedIcon: ImageVector,
     val unselectedItem: ImageVector,
-    val hasNotification:Boolean,
-    val badgeCount:Int?=null,
-    val ROUTE:String
+    val hasNotification: Boolean,
+    val badgeCount: Int? = null,
+    val ROUTE: String
 )
+
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             InsTeaTheme {
-
-
                 BottomNavigation()
             }
         }
@@ -72,11 +59,11 @@ class MainActivity : ComponentActivity() {
 
     @OptIn(ExperimentalMaterial3Api::class)
     @Composable
-    fun BottomNavigation(){
-        val navController= rememberNavController()
+    fun BottomNavigation() {
+        val navController = rememberNavController()
         val items = listOf(
             BottomNavItem(
-                title= "feed",
+                title = "feed",
                 selectedIcon = Icons.Filled.Home,
                 unselectedItem = Icons.Outlined.Home,
                 hasNotification = false,
@@ -85,7 +72,7 @@ class MainActivity : ComponentActivity() {
 
             ),
             BottomNavItem(
-                title= "profile",
+                title = "profile",
                 selectedIcon = Icons.Filled.Person,
                 unselectedItem = Icons.Outlined.Person,
                 hasNotification = true,
@@ -94,7 +81,7 @@ class MainActivity : ComponentActivity() {
 
             ),
             BottomNavItem(
-                title= "Notification",
+                title = "Notification",
                 selectedIcon = Icons.Filled.Notifications,
                 unselectedItem = Icons.Outlined.Notifications,
                 hasNotification = true,
@@ -111,7 +98,7 @@ class MainActivity : ComponentActivity() {
         Scaffold(
             bottomBar = {
                 NavigationBar {
-                    items.forEachIndexed{ index, item->
+                    items.forEachIndexed { index, item ->
                         NavigationBarItem(
                             selected = selectedItemIndex.value == index,
                             onClick = {
@@ -125,16 +112,18 @@ class MainActivity : ComponentActivity() {
                             icon = {
                                 BadgedBox(
                                     badge = {
-                                        if(item.badgeCount != null){
-                                            Text(text = item.badgeCount.toString(),
+                                        if (item.badgeCount != null) {
+                                            Text(
+                                                text = item.badgeCount.toString(),
                                                 fontSize = 12.sp,
 
                                                 modifier = Modifier
 
                                                     .padding(1.dp)
-                                                    .size(18.dp))
+                                                    .size(18.dp)
+                                            )
 
-                                        }else if(item.hasNotification){
+                                        } else if (item.hasNotification) {
                                             Badge()
                                         }
                                     }
@@ -143,8 +132,7 @@ class MainActivity : ComponentActivity() {
                                         imageVector =
                                         if (index == selectedItemIndex.value) {
                                             item.selectedIcon
-                                        }else item.unselectedItem
-                                        , contentDescription = item.title
+                                        } else item.unselectedItem, contentDescription = item.title
                                     )
                                 }
                             }
@@ -156,26 +144,24 @@ class MainActivity : ComponentActivity() {
         ) {
             NavHost(
                 navController = navController,
-                startDestination = SCREENS.FEED,
+                startDestination = SCREENS.PROFILE,
                 modifier = Modifier
-                    .padding(it))
+                    .padding(it)
+            )
             {
-                composable(route= SCREENS.FEED){
+                composable(route = SCREENS.FEED) {
                     Feed(navController = navController)
                 }
 
-                composable(route= SCREENS.PROFILE){
+                composable(route = SCREENS.PROFILE) {
                     Profile(navController = navController)
 
                 }
-                composable(route= SCREENS.NOTIFICATIONS){
+                composable(route = SCREENS.NOTIFICATIONS) {
                     Notificaiton(navController = navController)
                 }
-
             }
-
         }
 
     }
-
 }
