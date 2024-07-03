f88f17eb6e88ec57a501ccdbad972137222f0727
HAMMADQAdir <f1673673@gmail.com>
2024-07-03 10:50:43 +0530

    firebase auth

--- app/src/main/java/in/instea/instea/data/PostData.kt
+++ app/src/main/java/in/instea/instea/data/PostData.kt
@@ -1,7 +1,9 @@
 package `in`.instea.instea.data
 
 import android.location.Location
+import androidx.compose.runtime.Immutable
 
+@Immutable
 data class PostData(
     val name: String?,
     val location: String?,
