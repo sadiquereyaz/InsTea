67fe8aef3b17ab86b771786ef3dd72867dcbf8ca
Kashif Zafar <zafar.kashif04@gmail.com>
2024-07-15 20:38:57 +0530

    Signup View Model Done

--- app/src/main/java/in/instea/instea/screens/auth/Signup.kt
+++ app/src/main/java/in/instea/instea/screens/auth/Signup.kt
@@ -59,12 +59,9 @@ import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 import `in`.instea.instea.composable.DropdownMenuBox
 import `in`.instea.instea.data.AuthViewModel
 import `in`.instea.instea.data.DataSource.departments
@@ -553,7 +550,7 @@ fun Signup(
                     }
 
             },
-            isEnabled = true
+            isEnabled = btnEnabled
         )
         DividerTextComp()
         ScreenChangeText(modifier = Modifier, navController = navController)
