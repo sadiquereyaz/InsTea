a3c2f51d66c8c35f8559ad40665abb50297facd6
HAMMADQAdir <f1673673@gmail.com>
2024-08-24 11:43:28 +0530

    Search implemented

--- app/src/main/java/in/instea/instea/data/datamodel/Message.kt
+++ app/src/main/java/in/instea/instea/data/datamodel/Message.kt
@@ -2,10 +2,11 @@ package `in`.instea.instea.data.datamodel
 
 
 data class Message(
-    val message: String,
-    val timeStamp: DateAndHour,
-    val senderId: String,
-) {
+    val message: String="",
+    val timeStamp: DateAndHour=DateAndHour(),
+    val senderId: String="",
+)
+{
     constructor(message: String, senderId: String) : this(message, DateAndHour(), senderId)
 }
 
